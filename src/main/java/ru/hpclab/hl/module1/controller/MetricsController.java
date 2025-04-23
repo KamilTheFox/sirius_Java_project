1d6ed2843d7b4826220df4c606ac27f682df0f8c
FoxLover <100517471+KamilTheFox@users.noreply.github.com>
2025-04-24 01:32:52 +0300

    TestSuccessDockerImage

--- src/main/java/ru/hpclab/hl/module1/controller/MetricsController.java
+++ src/main/java/ru/hpclab/hl/module1/controller/MetricsController.java
@@ -1,10 +1,14 @@
 package ru.hpclab.hl.module1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.hpclab.hl.module1.service.*;
 
+@RestController
+@RequestMapping("/metrics")
 public class MetricsController
 {
-    @GetMapping("/metrics")
+    @GetMapping
     public ObservabilityService.MetricsSnapshot getMetrics()
     {
         return ObservabilityService.getMetricsAndClean();
