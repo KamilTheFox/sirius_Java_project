c39febfa5fdd42f6626ac9cd4ad932aedad639ae
FoxLover <100517471+KamilTheFox@users.noreply.github.com>
2025-04-25 07:18:04 +0300

    Delete Name endpoint

--- src/main/java/ru/hpclab/hl/module1/controller/RestaurantController.java
+++ src/main/java/ru/hpclab/hl/module1/controller/RestaurantController.java
@@ -40,13 +40,7 @@ public class RestaurantController
         return ResponseEntity.ok(restaurantService.getAllRestaurants());
     }
 
-    @GetMapping("/{name:^[a-zA-Z]+$}")
-    public ResponseEntity<Restaurant> getRestaurantByName(@PathVariable String name) {
-        Restaurant restaurant = restaurantService.getRestaurantByName(name);
-        return restaurant != null ? ResponseEntity.ok(restaurant) : ResponseEntity.notFound().build();
-    }
-
-    @GetMapping("/{id:^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$}")
+    @GetMapping("/{id}")
     public ResponseEntity<Restaurant> getRestaurantByID(@PathVariable UUID id) {
         Restaurant restaurant = restaurantService.getRestaurantByUUID(id);
         return restaurant != null ? ResponseEntity.ok(restaurant) : ResponseEntity.notFound().build();
