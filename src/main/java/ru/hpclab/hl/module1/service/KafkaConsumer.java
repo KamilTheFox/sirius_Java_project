8e6ab0a4276ece671aaf94b20945296d667dc864
FoxLover <100517471+KamilTheFox@users.noreply.github.com>
2025-05-01 12:05:58 +0300

    kafkaDel

--- src/main/java/ru/hpclab/hl/module1/service/KafkaConsumer.java
+++ src/main/java/ru/hpclab/hl/module1/service/KafkaConsumer.java
@@ -1,139 +1,139 @@
-package ru.hpclab.hl.module1.service;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
-import lombok.extern.slf4j.Slf4j;
-import ru.hpclab.hl.module1.dto.DishCreateDTO;
-import ru.hpclab.hl.module1.dto.KafkaMessage;
-import org.springframework.kafka.annotation.KafkaListener;
-import ru.hpclab.hl.module1.dto.OrderCreateDTO;
-import ru.hpclab.hl.module1.dto.RestaurantCreateDTO;
-import ru.hpclab.hl.module1.model.Dish;
-import ru.hpclab.hl.module1.model.Restaurant;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
-@Service
-@Slf4j
-public class KafkaConsumer {
-
-    private final ObjectMapper objectMapper;
-    private final RestaurantService restaurantService;
-    private final OrderService orderService;
-    private final DishService dishService;
-
-    public KafkaConsumer(ObjectMapper objectMapper,
-                         RestaurantService restaurantService,
-                         OrderService orderService,
-                         DishService dishService) {
-        this.objectMapper = objectMapper;
-        this.restaurantService = restaurantService;
-        this.orderService = orderService;
-        this.dishService = dishService;
-    }
-
-    @KafkaListener(topics = "var13",
-            groupId = "${spring.kafka.consumer.group-id}",
-            concurrency = "3",
-            containerFactory = "kafkaListenerContainerFactory")
-    public void consume(@Payload List<String> messages,
-                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-        for (int i = 0; i < messages.size(); i++) {
-            long offset = offsets.get(i);
-            String message = messages.get(i);
-            try {
-                KafkaMessage kafkaMessage = objectMapper.readValue(message, KafkaMessage.class);
-                if (offset % 500 == 0)
-                {
-                    log.info("500th message reached! Count: {}, Message: {}",
-                            offset, message);
-                }
-                consumeMain(kafkaMessage);
-            } catch (JsonProcessingException e) {
-                log.error("Error parsing Kafka message: {}", message, e);
-            } catch (Exception e) {
-                log.error("Error processing Kafka message: {}", message, e);
-            }
-        }
-    }
-
-    private void consumeMain(KafkaMessage message)
-    {
-        try {
-            switch (message.getEntity().toUpperCase()) {
-                case "RESTAURANT":
-                    handleRestaurant(message);
-                    break;
-                case "ORDER":
-                    handleOrder(message);
-                    break;
-                case "DISH":
-                    handleDish(message);
-                    break;
-                default:
-                    log.error("Unknown entity type: {}", message.getEntity());
-            }
-        } catch (Exception e) {
-            log.error("Error processing message: {}", message, e);
-        }
-    }
-
-    private void handleDish(KafkaMessage kafkaMessage) throws JsonProcessingException {
-        if ("DEL".equals(kafkaMessage.getOperation())) {
-            dishService.clearAll();
-        } else if ("POST".equals(kafkaMessage.getOperation())) {
-            DishCreateDTO dishDTO = objectMapper.convertValue(
-                    kafkaMessage.getPayload(),
-                    DishCreateDTO.class
-            );
-            dishService.addDish(
-                    new Dish(
-                            dishDTO.getName(),
-                            dishDTO.getPrice(),
-                            dishDTO.getWeight(),
-                            dishDTO.getRestaurantId())
-            );
-        }
-    }
-    private void handleOrder(KafkaMessage kafkaMessage) throws JsonProcessingException {
-        if ("DEL".equals(kafkaMessage.getOperation()))
-        {
-            orderService.clearAll();
-        }
-        else if ("POST".equals(kafkaMessage.getOperation()))
-        {
-            OrderCreateDTO orderDTO = objectMapper.convertValue(
-                    kafkaMessage.getPayload(),
-                    OrderCreateDTO.class
-            );
-
-            orderService.createOrder(
-                    restaurantService.getRestaurantByUUID(orderDTO.getRestaurant()),
-                    dishService.getDishesByIds(orderDTO.getDishes())
-                    ,orderDTO.getDeliveryAddress());
-        }
-    }
-
-    private void handleRestaurant(KafkaMessage message) throws JsonProcessingException {
-        if ("DEL".equals(message.getOperation()))
-        {
-            restaurantService.clearAll();
-        }
-        else if ("POST".equals(message.getOperation()))
-        {
-            RestaurantCreateDTO restaurant = objectMapper.convertValue(
-                    message.getPayload(),
-                    RestaurantCreateDTO.class
-            );
-            restaurantService.addRestaurant(
-                    new Restaurant(restaurant.getName(),
-                    restaurant.getCuisine(),
-                    restaurant.getMinimumOrder()));
-        }
-    }
-}
+//package ru.hpclab.hl.module1.service;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import org.springframework.kafka.support.KafkaHeaders;
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Service;
+//import lombok.extern.slf4j.Slf4j;
+//import ru.hpclab.hl.module1.dto.DishCreateDTO;
+//import ru.hpclab.hl.module1.dto.KafkaMessage;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import ru.hpclab.hl.module1.dto.OrderCreateDTO;
+//import ru.hpclab.hl.module1.dto.RestaurantCreateDTO;
+//import ru.hpclab.hl.module1.model.Dish;
+//import ru.hpclab.hl.module1.model.Restaurant;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+//
+//import java.util.List;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//@Service
+//@Slf4j
+//public class KafkaConsumer {
+//
+//    private final ObjectMapper objectMapper;
+//    private final RestaurantService restaurantService;
+//    private final OrderService orderService;
+//    private final DishService dishService;
+//
+//    public KafkaConsumer(ObjectMapper objectMapper,
+//                         RestaurantService restaurantService,
+//                         OrderService orderService,
+//                         DishService dishService) {
+//        this.objectMapper = objectMapper;
+//        this.restaurantService = restaurantService;
+//        this.orderService = orderService;
+//        this.dishService = dishService;
+//    }
+//
+//    @KafkaListener(topics = "var13",
+//            groupId = "${spring.kafka.consumer.group-id}",
+//            concurrency = "3",
+//            containerFactory = "kafkaListenerContainerFactory")
+//    public void consume(@Payload List<String> messages,
+//                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+//        for (int i = 0; i < messages.size(); i++) {
+//            long offset = offsets.get(i);
+//            String message = messages.get(i);
+//            try {
+//                KafkaMessage kafkaMessage = objectMapper.readValue(message, KafkaMessage.class);
+//                if (offset % 500 == 0)
+//                {
+//                    log.info("500th message reached! Count: {}, Message: {}",
+//                            offset, message);
+//                }
+//                consumeMain(kafkaMessage);
+//            } catch (JsonProcessingException e) {
+//                log.error("Error parsing Kafka message: {}", message, e);
+//            } catch (Exception e) {
+//                log.error("Error processing Kafka message: {}", message, e);
+//            }
+//        }
+//    }
+//
+//    private void consumeMain(KafkaMessage message)
+//    {
+//        try {
+//            switch (message.getEntity().toUpperCase()) {
+//                case "RESTAURANT":
+//                    handleRestaurant(message);
+//                    break;
+//                case "ORDER":
+//                    handleOrder(message);
+//                    break;
+//                case "DISH":
+//                    handleDish(message);
+//                    break;
+//                default:
+//                    log.error("Unknown entity type: {}", message.getEntity());
+//            }
+//        } catch (Exception e) {
+//            log.error("Error processing message: {}", message, e);
+//        }
+//    }
+//
+//    private void handleDish(KafkaMessage kafkaMessage) throws JsonProcessingException {
+//        if ("DEL".equals(kafkaMessage.getOperation())) {
+//            dishService.clearAll();
+//        } else if ("POST".equals(kafkaMessage.getOperation())) {
+//            DishCreateDTO dishDTO = objectMapper.convertValue(
+//                    kafkaMessage.getPayload(),
+//                    DishCreateDTO.class
+//            );
+//            dishService.addDish(
+//                    new Dish(
+//                            dishDTO.getName(),
+//                            dishDTO.getPrice(),
+//                            dishDTO.getWeight(),
+//                            dishDTO.getRestaurantId())
+//            );
+//        }
+//    }
+//    private void handleOrder(KafkaMessage kafkaMessage) throws JsonProcessingException {
+//        if ("DEL".equals(kafkaMessage.getOperation()))
+//        {
+//            orderService.clearAll();
+//        }
+//        else if ("POST".equals(kafkaMessage.getOperation()))
+//        {
+//            OrderCreateDTO orderDTO = objectMapper.convertValue(
+//                    kafkaMessage.getPayload(),
+//                    OrderCreateDTO.class
+//            );
+//
+//            orderService.createOrder(
+//                    restaurantService.getRestaurantByUUID(orderDTO.getRestaurant()),
+//                    dishService.getDishesByIds(orderDTO.getDishes())
+//                    ,orderDTO.getDeliveryAddress());
+//        }
+//    }
+//
+//    private void handleRestaurant(KafkaMessage message) throws JsonProcessingException {
+//        if ("DEL".equals(message.getOperation()))
+//        {
+//            restaurantService.clearAll();
+//        }
+//        else if ("POST".equals(message.getOperation()))
+//        {
+//            RestaurantCreateDTO restaurant = objectMapper.convertValue(
+//                    message.getPayload(),
+//                    RestaurantCreateDTO.class
+//            );
+//            restaurantService.addRestaurant(
+//                    new Restaurant(restaurant.getName(),
+//                    restaurant.getCuisine(),
+//                    restaurant.getMinimumOrder()));
+//        }
+//    }
+//}
